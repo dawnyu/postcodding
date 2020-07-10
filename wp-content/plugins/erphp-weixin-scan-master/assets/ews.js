83c85b4b0a2c2610f527240357002518093c56bf
dawnyu <yuleiming09367@hellobike.com>
2020-07-10 11:31:50 +0800

    fix

--- wp-content/plugins/erphp-weixin-scan-master/assets/ews.js
+++ wp-content/plugins/erphp-weixin-scan-master/assets/ews.js
@@ -21,7 +21,7 @@ jQuery(function($){
                     if(data.status == "1"){
                         var obj = parseUrl(window.location.href);
                         if (obj.redirect_to) {
-                          history.go(-2);
+                          history.back();
                         } else {
                           location.reload();
                         }
